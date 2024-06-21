5c00bc573dc5be47c3b7a4a9d6f7bba1f5195744
sggarci4 <159163227+sggarci4@users.noreply.github.com>
2024-06-21 17:36:13 -0600

    Add files via upload

---
+++ WebACT2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebACT2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Menu");
+        }
+        public ActionResult IrVistaNombre()
+        {
+            return View("FormNombre");
+        }
+        public ActionResult IrVistaDireccion()
+        {
+            return View("FormDireccion");
+        }
+        public ActionResult RecibirNombre(string nombre, string apellP, string apellM, int edad)
+        {
+            string oracion = $"Mi nombre es: {nombre} {apellP} {apellM} y tengo {edad} años.";
+
+            TempData["oracion"] = oracion;
+
+            return View("FormNombre");
+        }
+        public ActionResult RecibirDireccion(string direccion, int cp, string colonia,string delegacion)
+        {
+            string oracion = $"Mi direccion es: {direccion}, {cp}, {colonia}, {delegacion}.";
+
+            TempData["oracion"] = oracion;
+
+            return View("ResultadoFormDireccion");
+        }
+    }
+}
---
+++ WebACT2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebACT2.MvcApplication" Language="C#" %>
